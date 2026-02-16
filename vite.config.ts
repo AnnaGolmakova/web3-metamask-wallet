0380b5cdb517dbd00f6e75364f4b34d7ed7668c6
Anna Golmakova <ann.golmakova@gmail.com>
2026-02-16 21:30:12 +0000

    Publish to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base:
+    mode === "development"
+      ? "/"
+      : "https://annagolmakova.github.io/web3-metamask-wallet/",
   plugins: [react()],
-})
+}));
